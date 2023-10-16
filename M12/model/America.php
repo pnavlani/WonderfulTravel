4d6c836c48308ce23a10855148e0a8ab5d943231
AymanSbayy <132548449+AymanSbayy@users.noreply.github.com>
2023-10-16 16:09:50 +0200

    Add files via upload

---
+++ M12/model/America.php
@@ -0,0 +1,29 @@
+<?php
+function getAmerica()
+{
+    require 'database/pdo.php';
+    $conn = connexion();
+
+    $sql = "SELECT Ciutat, Preu FROM ciutats WHERE Continent_Id = 1";
+    $stmt = $conn->prepare($sql);
+    $stmt->execute();
+    $america = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    $ciutat = array();
+    $preu = array();
+    foreach ($america as $key => $value) {
+        array_push($ciutat, $value['Ciutat']);
+        array_push($preu, $value['Preu']);
+    }
+    $html = "";
+    for ($i = 0; $i < count($ciutat); $i++) {
+        $html .= "<div class='ciutat'>";
+        $html .= "<img src='../photos/" . $ciutat[$i] . ".jpg' alt='" . $ciutat[$i] . "' width='200' height='200'>";
+        $html .= "<p>" . $ciutat[$i] . "</p>";
+        $html .= "<p>" . $preu[$i] . "€</p>";
+        $html .= "</div>";
+    }
+    return $html;
+}
+
+?>
